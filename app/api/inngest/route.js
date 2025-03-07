ec113ef4f0acba2c2724ce6f306ddffc4cb26566
unknown <sidhantjha2002@gmail.com>
2025-03-07 16:57:16 +0530

    first commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,7 @@
-import { inngest } from "@/lib/inngest/client";
-import { serve } from "inngest/next";
 
+import { inngest } from "@/lib/inngest/client";
+import { generateIndustryInsights } from "@/lib/inngest/functions";
+import { serve } from "inngest/next";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
